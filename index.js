b86768b1794dcaf5a1f19ba684efa38b360af548
Ryan Schniederjan <ryno@schniteke.com>
2021-02-23 14:04:29 -0600

    Added Browser interface

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 'use strict'
-import { createStore } from 'redux';
+// import { createStore } from 'redux';
+
+const { createStore } = Redux;
 
 console.log("Yo Joe!");
 
@@ -33,7 +35,10 @@ const account = (state = defaultState, action) => {
   }
 }
 
-const store = createStore(account);
+const store = createStore(
+  account,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 console.log('the store is', store);
 
@@ -41,6 +46,19 @@ store.subscribe(() => {
   console.log('subscribing to state changes..');
   const state = store.getState();
   console.log("the state is:", state);
+  const balance = document.querySelector('#balance');
+  balance.innerHTML = state.balance;
 });
 
-store.dispatch(actionIncrement);
+const incrementButton = document.querySelector('#add');
+const decrementButton = document.querySelector('#subtract');
+
+incrementButton.addEventListener("click", (e) => {
+  e.preventDefault;
+  store.dispatch(actionIncrement);
+});
+
+decrementButton.addEventListener("click", (e) => {
+  e.preventDefault;
+  store.dispatch(actionDecrement);
+});
